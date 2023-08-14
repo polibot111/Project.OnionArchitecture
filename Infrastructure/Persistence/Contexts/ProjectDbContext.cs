1961a3cda1e0259b4214a341c409628f02de2ec7
Furkan kochan <furkan.kochan10@gmail.com>
2023-08-14 16:22:28 +0300

    Database Connection Finished

---
+++ Infrastructure/Persistence/Contexts/ProjectDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<RestaurantBranches> RestorantBranches { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RestaurantBranches>()
+                .ToTable("restaurant_branches");
+        }
+    }
+}
