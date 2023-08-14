16a60a42980ec5476e667860db1a81a2fc8e9a92
Furkan kochan <furkan.kochan10@gmail.com>
2023-08-14 23:30:27 +0300

    Seq added

--- Presentation/API/Program.cs
+++ Presentation/API/Program.cs
@@ -1,6 +1,8 @@
 using Application;
 using Infrastructure;
 using Persistence;
+using Serilog;
+using Serilog.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Logger log = new LoggerConfiguration()
+    .WriteTo.Seq(builder.Configuration["Seq:ServerUrl"])
+    .Enrich.FromLogContext()
+    .MinimumLevel.Information()
+    .CreateLogger();
+
+builder.Host.UseSerilog(log);
 
 var app = builder.Build();
 
